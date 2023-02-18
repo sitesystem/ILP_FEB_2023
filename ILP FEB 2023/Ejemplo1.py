49800fc63a2f3f2d318643e3fbace2206b42c664
sitesystem <l_aloaxa05@hotmail.com>
2023-02-18 12:46:49 -0600

    Primera subida

---
+++ ILP FEB 2023/Ejemplo1.py
@@ -0,0 +1,13 @@
+# COMENTARIOS: Sirven para detallar o documentar nuestro programa
+# A los comentarios los omite o no los toma en cuenta Python
+
+# Imprimiendo o mostrando en pantalla resultados
+print("Hola a todos!!!")
+print('Eduardo');
+print(15+20) # Operación de Suma
+
+print(15-6)
+
+
+
+
